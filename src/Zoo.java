72cc07d36e610e63e67d1da57cb06fff42350bf0
b242290020-bit <b242290020@gmail.com>
2026-04-08 23:38:10 +0800

    Update Zoo.java

--- src/Zoo.java
+++ src/Zoo.java
@@ -31,7 +31,9 @@ public class Zoo<T extends Animal> {
 
 	// Хэрэгжүүл: transferAnimals
 	public void transferAnimals(List<? extends T> source, List<? super T> destination) {
-		// Бүх амьтдыг source-оос destination руу шилжүүл
+		for (T animal : source) {
+			destination.add(animal);
+		}
 	}
 
 	public List<T> getAnimals() {
